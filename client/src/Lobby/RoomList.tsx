1e76115f4b9d715902546780e920001ea0b002cb
juhyun <dydgla36@gmail.com>
2023-07-23 23:14:46 +0900

    [완료] 로비에서 방만들기 성공!!

--- client/src/Lobby/RoomList.tsx
+++ client/src/Lobby/RoomList.tsx
@@ -4,25 +4,28 @@ import axios from "axios";
 import RoomInfo from "./RoomInfo";
 import RoomCreate from "./RoomCreate";
 interface RoomData {
-  idx: number;
+  idx: string;
   room_name: string;
 }
 
 const RoomList = () => {
   const [title, setTitle] = useState<string[]>([""]);
   const [selectedTitle, setSelectedTitle] = useState("");
+  const [idx, setIdx] = useState("");
   const [flag, setFlag] = useState<number>(0);
   const [currentRoomIdx, setCurrentRoomIdx] = useState(0);
+  const [currentIdx, setCurrentIdx] = useState("");
   
   useEffect(() => {
     handleRoomList();
   }, []);
-
+  
   useEffect(() => {
-    if (title.length > 0) {
+    if (title.length > 0 && idx.length > 0) {
       setSelectedTitle(title[0]);
+      setCurrentIdx(idx[0]);
     }
-  }, [title]);
+  }, [title, idx]);
   
   const handleRoomList = async () => {
     try {
@@ -30,13 +33,12 @@ const RoomList = () => {
         `${process.env.REACT_APP_API_URL}/lobby/roomList`
         );
         console.log(response.data);
-      
+        
         setTitle(response.data.map((room: RoomData) => room.room_name));
-      
-      // alert("방 정보 출력 성공");
+        setIdx(response.data.map((idx: RoomData) => idx.idx));
+  
     } catch (error) {
       console.error("방 정보 출력 오류:", error);
-      // alert("방 정보 출력 실패");
     }
   };
 
@@ -48,12 +50,14 @@ const RoomList = () => {
     if (currentRoomIdx < title.length - 1) {
       setSelectedTitle(title[currentRoomIdx + 1]);
       setCurrentRoomIdx((prevIdx) => prevIdx + 1);
+      setCurrentIdx((idx[currentRoomIdx+1]));
     }
   };
   const handlePrevClick = () => {
     if (currentRoomIdx > 0) {
       setSelectedTitle(title[currentRoomIdx - 1]);
       setCurrentRoomIdx((prevIdx) => prevIdx - 1);
+      setCurrentIdx((idx[currentRoomIdx - 1]));
     }
   };
   return (
@@ -66,7 +70,7 @@ const RoomList = () => {
         </ul>
       </div>
       <div style={{position : "fixed", marginLeft : "30%" , marginTop : "0%"}}>
-        {flag === 0 && <RoomInfo selectedTitle={selectedTitle} />}
+        {flag === 0 && <RoomInfo selectedTitle={selectedTitle} currentIdx= {currentIdx}/>}
         {flag !== 0 && (<RoomCreate />)}
       </div>
       <div>
