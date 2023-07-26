1cc827220f619cde1e54b3fde667780a01b183a0
kyu3638 <kyu3638@gmail.com>
2023-07-26 17:39:42 +0900

    [진행중]css최소화,깔끔하게 정리

--- client/src/Lobby/RoomCreate.tsx
+++ client/src/Lobby/RoomCreate.tsx
@@ -1,10 +1,9 @@
-import React, {useState} from 'react';
-import styles from './RoomCreate.module.css';
-import axios from 'axios';
-import {useNavigate} from 'react-router-dom';
+import React, { useState } from "react";
+import styles from "./RoomCreate.module.css";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const RoomCreate = () => {
-
   const [room_name, setRoomName] = useState<string>("");
   const [idx, setIdx] = useState<string>("");
   const navigate = useNavigate();
@@ -12,26 +11,34 @@ const RoomCreate = () => {
   const handleRoomCreate = async (e: React.FormEvent) => {
     e.preventDefault();
     try {
-      const response = await axios.post(`${process.env.REACT_APP_API_URL}/lobby/create`, {
-        room_name,
-        idx,
-      });
+      const response = await axios.post(
+        `${process.env.REACT_APP_API_URL}/lobby/create`,
+        {
+          room_name,
+          idx,
+        }
+      );
       console.log(response.data);
       alert("방 생성 성공");
-      navigate('/Room', { state: { roomName: room_name , idx : idx} }); 
+      navigate("/Room", { state: { roomName: room_name, idx: idx } });
     } catch (error) {
-        console.error("방 생성 오류:", error);
-        alert("방 생성 실패");
+      console.error("방 생성 오류:", error);
+      alert("방 생성 실패");
     }
   };
   return (
-      <div className={styles.container}>
-        <div>
-          <input type = "text" placeholder="방 제목" value={room_name} onChange={e => setRoomName(e.target.value)}/>
-        </div>
-        <button type="submit" onClick = {handleRoomCreate}>방 생성</button>
-      </div>
-    )
-}
+    <div className={styles.roomCreateWrap}>
+      <input
+        type="text"
+        placeholder="방 제목"
+        value={room_name}
+        onChange={(e) => setRoomName(e.target.value)}
+      />
+      <button type="submit" onClick={handleRoomCreate}>
+        방 생성
+      </button>
+    </div>
+  );
+};
 
-export default RoomCreate
+export default RoomCreate;
--- client/src/Lobby/UserList.tsx
+++ client/src/Lobby/UserList.tsx
@@ -4,7 +4,7 @@ import styles from './UserList.module.css';
 const UserList: React.FC = () => {
     const users = ['User 1', 'User 2', 'User 3', 'User 4', 'User 5'];
     return (
-        <div className={styles.position}>
+        <div className={styles.userListWrap}>
             <ul>
                 {users.map((user, idx) => (
                     <li key={idx} className="userItem">{user} {/* ON,OFF 확인 색깔 */} </li>
