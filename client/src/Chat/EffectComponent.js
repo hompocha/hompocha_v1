25a34f340cb15888771177eb47d91275d1d15f8b
juhyun <dydgla36@gmail.com>
2023-08-04 16:40:08 +0900

    [완료] 뭐먹을까 하면 음식 스탬프 쾅

--- client/src/Chat/EffectComponent.js
+++ client/src/Chat/EffectComponent.js
@@ -3,6 +3,7 @@ import CherryBlossom from "../keyword/cherryBlossom";
 import CloudCanvas from "../keyword/cloud";
 import CatCanvas from "../keyword/cat";
 import DogCanvas from "../keyword/Dog";
+import FoodCanvas from "../keyword/food";
 
 const EffectComponent = ({ user, sessionConnected }) => {
   const [keywordList, setKeywordList] = useState({
@@ -11,18 +12,30 @@ const EffectComponent = ({ user, sessionConnected }) => {
     구름: [],
     벚꽃: [],
     그만해: [],
+    뭐먹을까: [],
   });
 
   useEffect(() => {
     const handleEffect = (event) => {
       const data = event.data;
-      setKeywordList((prevKeywordList) => ({
-        ...prevKeywordList,
-        [data]: [
-          ...prevKeywordList[data],
-          { x: Math.random() * 100, y: Math.random() * 100 },
-        ],
-      }));
+      if (data === "뭐 먹을까" || data === "뭐먹을까") {
+        setKeywordList((prevKeywordList) => ({
+          ...prevKeywordList,
+          뭐먹을까: [
+            ...prevKeywordList.뭐먹을까,
+            { x: Math.random() * 100, y: Math.random() * 100 },
+          ],
+        }));
+      } else {
+        setKeywordList((prevKeywordList) => ({
+          ...prevKeywordList,
+          [data]: [
+            ...prevKeywordList[data],
+            { x: Math.random() * 100, y: Math.random() * 100 },
+          ],
+        }));
+      }
+      
     };
     if (sessionConnected) {
       const streamManager = user.getStreamManager().stream.session;
@@ -60,6 +73,13 @@ const EffectComponent = ({ user, sessionConnected }) => {
         <React.Fragment>
           {keywordList.벚꽃.map((blossom, index) => (
             <CherryBlossom key={index} x={blossom.x} y={blossom.y} />
+          ))}
+        </React.Fragment>
+      )}
+      {!stopRequested && (
+        <React.Fragment>
+          {keywordList.뭐먹을까.map((food, index) => (
+            <FoodCanvas key={index} x={food.x} y={food.y} />
           ))}
         </React.Fragment>
       )}
