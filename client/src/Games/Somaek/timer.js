640e7d34efabbc9f01334ad1f599e1380f64e0d5
ddaogi <47049582+ddaogi@users.noreply.github.com>
2023-08-09 01:42:46 +0900

    Update timer.js

--- client/src/Games/Somaek/timer.js
+++ client/src/Games/Somaek/timer.js
@@ -4,7 +4,6 @@ import { useState, useEffect } from "react";
 import styles from "./timer.module.css";
 
 const TimerBar = ({ timeMax, gameEnd, start} = {}) => {
-  // const barWidth = (timeLeft / timeMax) * 100;
   const defaultTime = timeMax;
   const [timeleft, setTimeleft] = useState(defaultTime);
 
@@ -26,7 +25,6 @@ const TimerBar = ({ timeMax, gameEnd, start} = {}) => {
 
   return (
     <div>
-        {/* <img src="../../timer.png" alt="timer"/> */}
         <div className={styles.stopWatch}></div>
         <div className={styles.timeBar}>
         <div className={styles.bar} style={{ width: `${(timeleft/timeMax)*100}%` }}></div>
@@ -38,54 +36,6 @@ const TimerBar = ({ timeMax, gameEnd, start} = {}) => {
 
 export {TimerBar};
 
-// import React, { useState, useEffect } from "react";
-// import {styles} from "./timer.module.css";
-// const Timer = ({gameTime}) => {
-//   const [time, setTime] = useState(gameTime);
-//   const [isRunning, setIsRunning] = useState(true);
 
-//   useEffect(() => {
-//     let intervalId;
-//     if (isRunning) {
-//       intervalId = setInterval(() => setTime(time - 1), 10);
-//     }
-//     return () => clearInterval(intervalId);
-//   }, [isRunning, time]);
-
-
-//   // Seconds calculation
-//   const seconds = Math.floor((time % 6000) / 100);
-
-//   // Milliseconds calculation
-//   const milliseconds = time % 100;
-
-//   // Method to start and stop timer
-//   const startAndStop = () => {
-//     setIsRunning(!isRunning);
-//   };
-
-//   // Method to reset timer back to 0
-//   const reset = () => {
-//     setTime(gameTime);
-//   };
-//   return (
-//     <div className="stopwatch-container">
-//       <p className="stopwatch-time">
-//         {seconds.toString().padStart(2, "0")}:
-//         {milliseconds.toString().padStart(2, "0")}
-//       </p>
-//       {/* <div className="stopwatch-buttons">
-//         <button className="stopwatch-button" onClick={startAndStop}>
-//           {isRunning ? "Stop" : "Start"}
-//         </button>
-//         <button className="stopwatch-button" onClick={reset}>
-//           Reset
-//         </button>
-//       </div> */}
-//     </div>
-//   );
-// };
-
-// export {Timer};
 
 
